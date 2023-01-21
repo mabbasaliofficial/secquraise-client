3ec3138fc20b65e9d9b189a1561f4268b919ec1b
mabbasaliofficial <mabbasaliofficial@gmail.com>
2023-01-21 22:48:54 +0600

    sandwich filter added

--- src/Components/RightSideNav.js
+++ src/Components/RightSideNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 import { BiMenuAltRight } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
@@ -15,13 +16,86 @@ const RightSideNav = () => {
   }, []);
 
   return (
-    <div>
-      <div className="flex justify-between">
+    <div className="overflow-y-auto h-screen">
+      <div className="flex justify-between sticky top-0 bg-white py-2 px-2">
         <h3 className="text-left text-xl font-bold">Events</h3>
-        <BiMenuAltRight className="font-bold mt-1 text-3xl border-gray-900 border-2" />
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0}>
+            <BiMenuAltRight className="font-bold mt-1 text-3xl border-gray-900 border-2" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-[350px]"
+          >
+            <div className="mx-10 my-2">
+              <h3 className="font-bold">Location</h3>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Chennai</span>
+                  <input
+                    type="radio"
+                    name="location-radio"
+                    className="radio radio-info checked:bg-blue-500"
+                    checked
+                  />
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Hyderabad</span>
+                  <input
+                    type="radio"
+                    name="location-radio"
+                    className="radio radio-info checked:bg-blue-500"
+                    checked
+                  />
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Bangalore</span>
+                  <input
+                    type="radio"
+                    name="location-radio"
+                    className="radio radio-info checked:bg-blue-500"
+                    checked
+                  />
+                </label>
+              </div>
+            </div>
+            <div className="mx-10 my-2">
+            <h3 className="font-bold">Gender</h3>
+            <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Male</span>
+                  <input
+                    type="radio"
+                    name="gender-radio"
+                    className="radio radio-secondary checked:bg-red-500"
+                    checked
+                  />
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Female</span>
+                  <input
+                    type="radio"
+                    name="gender-radio"
+                    className="radio radio-secondary checked:bg-red-500"
+                    checked
+                  />
+                </label>
+              </div>
+            </div>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
+        </div>
       </div>
       {data.map((dt) => (
-        <div className="bg-gray-400 m-2 text-white rounded p-1">
+        <div className="bg-gray-400 m-3 text-white rounded p-1">
           <Link to={`/data/${dt.id}`}>
             <div className="px-2">
               <div className="flex justify-between">
--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -5,10 +5,10 @@ import RightSideNav from "../Components/RightSideNav";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="">
       <Navbar />
       <div class="grid grid-cols-12">
-        <div class="col-span-8 border-solid border-2 border-indigo-600 m-5">
+        <div class="col-span-8  m-5">
             <Outlet/>
         </div>
         <div class="col-span-4 border-solid border-4 border-gray-400 px-4">
